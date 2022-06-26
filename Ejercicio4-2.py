d62b9c567d90575619c1bd6f414ff5b78f62e89c
Fabricio Navarro <Navarro.fa998@gmail.com>
2022-06-25 23:32:37 -0300

    Resolución

---
+++ Ejercicio4-2.py
@@ -0,0 +1,8 @@
+numero_inicial = 2
+numero_final = 8 
+
+#                  [numero_inicial, numero_final]
+for numero in range(numero_inicial, numero_final + 1):
+    # Comprobación de número impar
+    if (numero % 2 != 0):
+        print(numero)
